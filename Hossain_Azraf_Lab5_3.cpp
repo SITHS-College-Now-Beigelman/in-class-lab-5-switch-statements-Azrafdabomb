609b8a2c877ead1aa66ea95c0beeeb42aaaf8cec
Azrafdabomb <azrafh@nycstudents.net>
2024-10-13 16:48:57 -0400

    Update Hossain_Azraf_Lab5_3.cpp

--- Hossain_Azraf_Lab5_3.cpp
+++ Hossain_Azraf_Lab5_3.cpp
@@ -14,7 +14,7 @@ int main()
     int x = 0, y = 0, input = 25;
     srand(time(0));
 
-/*    int i;
+    int i;
     for(i=1; i <= input; i++)
     {
         int direct;
@@ -41,34 +41,33 @@ int main()
     }
     cout << x << ' ' <<  y << endl;
     // Robot's placement after 25 steps, -1 -6 (one step south and six steps west of the starting point)
-*/
+
+    cout << '1' << endl;
     x = 0;
     y = 0;
     int direct;
     direct = rand() % 4 + 1; //random number between 1 and 4 so each is 25% //probable
+
     int steps = 0;
-    while ((x != 2) && (y != 3))
+    while (!(x == 2 && y == 3)) 
     {    
         switch(direct)
         {
             case 1:
-                steps = steps + 1;
                 y = y + 1;
-
+                break;
             case 2:
-                steps = steps + 1;
                 y = y - 1;
-            
+                break;
             case 3:
-                steps = steps + 1;
                 x = x + 1;
-                
+                break;
             case 4:
-                steps = steps + 1;
                 x = x - 1;
-                
+                break;
         }
+    steps = steps + 1;
     }
-    cout << steps;
+    cout << steps << "    " << x << ' ' << y;
     return 0;
 }
